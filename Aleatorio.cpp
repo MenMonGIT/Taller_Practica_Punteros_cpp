7e4344ba237a45ea6d15239db5c4da409b3e6d20
MenMonGIT <meneses.julian@correounivalle.edu.co>
2025-05-02 03:07:48 +0000

    ligero cambio

--- Aleatorio.cpp
+++ Aleatorio.cpp
@@ -12,7 +12,7 @@ Aleatorio::Aleatorio(){
 void Aleatorio::generarPosicionValida(int laberinto[][10], int filas, int columnas, int& x, int& y) {
   //int& x y int&y son referencias que devolveran los valores a las variables externas
     do {
-        x = rand() % filas; // elige una fila aleatoria entre 0 y filas-1
-        y = rand() % columnas; // elige una columna aleatoria entre 0 y columnas-1
+        x = rand() % 10; // elige una fila aleatoria entre 0 y filas-1
+        y = rand() % 10; // elige una columna aleatoria entre 0 y columnas-1
     } while (laberinto[x][y] == 0); // evita caer en una celda "precipicio", repitiendo en caso de que el valor de la celda sea 0
 }
--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,16 @@
 #include "Avatar.h"
 #include "Menus.h"
+#include "Aleatorio.h"
 int main(){
    // Avatar avatar1("julian");
     Menus menus;
     menus.inicio("julian");
 
     Tablero t;
-
+    Aleatorio aleatorio1;
     t.imprimir(0,0);
+    //pruebas para aleatorio
+    //aleatorio1.generarPosicionValida();
+
+
 }
