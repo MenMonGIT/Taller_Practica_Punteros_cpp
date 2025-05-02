a545501b09008554d858cdcbd1d3c45ac32a5875
Orie <LaNanaXD@outlook.com>
2025-05-01 19:26:59 -0500

    Update Aleatorio.cpp
    Se añaden los métodos y las librerías para que funcionen

--- Aleatorio.cpp
+++ Aleatorio.cpp
@@ -1,7 +1,14 @@
 #include "Aleatorio.h"
-
+#include <cstdlib>
+#include <ctime>
 
 Aleatorio::Aleatorio(){
+  std::srand(std::time(0)); // inicializar la semilla aleatoria una vez
+}
 
-
+void Aleatorio::generarPosicionValida(int laberinto[][10], int filas, int columnas, int& x, int& y) {
+    do {
+        x = rand() % filas;
+        y = rand() % columnas;
+    } while (laberinto[x][y] == 0); // evita caer en una celda "precipicio"
 }
